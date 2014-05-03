a36eb1076251e67db61f518ab4bf4bb88acec10d
Jared Poetter <poet0007@umn.edu>
2014-05-02 20:56:39 -0500

    Now you can specify the width and height of the tool. This is going need to be done manually for each tool. Each tool is going to need to have the values set to the aspect ratio that you want to have for the tool look correct.

--- Version 2/javascript/measurementTools.js
+++ Version 2/javascript/measurementTools.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 	var rulerSizeReduction = 8;
 	//var containX = 600;
 	//var containY = 800;
-	var ruler = $('#over');
+	var ruler = $('#horizontalRulerID');
 	ruler.draggable({containment:"body"});//does not allow ruler to be moved outside of the window, this fixes the scrolling problem that is caused by dragging the ruler offscreen
-	ruler.height($( document ).height() / rulerSizeReduction);
-	ruler.width($( document ).width() / rulerSizeReduction);
+	//ruler.height($( window ).height() / rulerSizeReduction);
+	//ruler.width($( window ).width() / rulerSizeReduction);
 	ruler.resizable({aspectRatio:true});
 	ruler.draggable({containment:"body"});//does not allow ruler to be moved outside of the window, this fixes the scrolling problem that is caused by dragging the ruler offscreen
 	//ruler.draggable();
