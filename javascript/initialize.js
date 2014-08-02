a5e2121a1e67ba02761fad8767a975eca24a6747
Jared Poetter <poet0007@umn.edu>
2014-08-02 10:21:15 -0500

    moved document.ready code into one file for easier debugging FIXED: chrome showing the 'click to play' image showing up when the user tries to go to the previous frames

--- javascript/initialize.js
+++ javascript/initialize.js
@@ -1,2 +1,265 @@
 //This is the final javascript file that get executed
 
+$(document).ready(function(){
+	//----------------------------------------------//
+	//Player Controls                               //
+	//----------------------------------------------//
+	//Removing the reset tools buttons if it is turned off
+	if(!resetButton){
+		$("#resetTools").css("display", "none");
+	}
+	
+	//Drop down for the different videos
+	var video_selection = $("#video_selection");
+	var video_selector = $("#video_selector");
+	
+	//Removing the Video Selector if it is turned off
+	if(!videoSelection){
+		video_selection.css("display", "none");
+	}
+	
+	//Setting the default video
+	var dmv_player = document.getElementById("dmv_video");
+	var mp4_video = document.getElementById("mp4_video");
+	var ogg_video = document.getElementById("ogg_video");
+	
+	//Setting the oncanplaythrough callback to change the video poster image to the 'click to play'
+	var showedPlaySplashScreen = false;
+	dmv_player.oncanplaythrough = 
+		function(){
+			if(!showedPlaySplashScreen){
+				console.log("video ready!");
+				dmv_player.setAttribute('poster', '../images/play_splash_screen.png');
+				showedPlaySplashScreen = true;
+			}
+		};
+	
+	//Setting the play callback to remove the video poster of 'click to play'
+	$('#dmv_video').bind('play', function(){dmv_player.removeAttribute('poster');});
+	
+	//Setting the video sources
+	if(dmv_player.canPlayType("video/ogg") == "maybe" || dmv_player.canPlayType("video/ogg") == "probably") {
+		$(ogg_video).attr('src', videoArray[0][_locationOGV]);
+	}
+	else if(dmv_player.canPlayType("video/mp4") == "maybe" || dmv_player.canPlayType("video/mp4") == "probably") {
+		$(mp4_video).attr('src', videoArray[0][_locationMP4]);
+	}
+	
+	//Reload the dmv video
+	dmv_player.load();
+	
+	//Setting up the options in the video selection
+	var index = 0;
+	for(element in videoArray){
+		var option = video_selector.append($("<option></option>").attr("value",index).text(videoArray[index][_nameOfVideo]));
+		index++;
+	}
+	
+	//If the drop down changes
+	video_selector.change(function(){
+		var video_index = document.getElementById("video_selector").selectedIndex;
+		var dmv_player = document.getElementById("dmv_video");
+		var mp4_video = document.getElementById("mp4_video");
+		var ogg_video = document.getElementById("ogg_video");
+		
+		//Setting the video sources
+		if(dmv_player.canPlayType("video/ogg") == "maybe" || dmv_player.canPlayType("video/ogg") == "probably") {
+			$(ogg_video).attr('src', videoArray[video_index][_locationOGV]);
+		}
+		else if(dmv_player.canPlayType("video/mp4") == "maybe" || dmv_player.canPlayType("video/mp4") == "probably") {
+			$(mp4_video).attr('src', videoArray[video_index][_locationMP4]);
+		}
+		
+		//Reload the dmv video
+		dmv_player.load();
+		
+		//Setting the slider back to the beginning
+		$("#slider").slider('value',0);
+		
+		//Hiding the overlay image
+		overlayImageVisible = false;
+		$("#overlayImageID").css("display", "none");
+		//Making sure the video is visible
+		$("#dmv_video").css("display", "initial");
+		//Switching the overlay image
+		document.getElementById("overlayImageID").src = videoArray[document.getElementById("video_selector").selectedIndex][_overlayImage];
+	});
+	
+	//----------------------------------------------//
+	//Measurement Tools                             //
+	//----------------------------------------------//
+	//Setting up the different tools
+	//Adding in the tools dynamically from the toolsArray
+	var index = 0;
+	//Getting the main div
+	var mainDiv = document.getElementById("main");
+	//Tool Button Table
+	var toolButtonTable = document.getElementById("button_table_2");
+	//Going through all the tools defined in the 
+	for(elements in toolsArray){
+		//HTML code for the tools
+		var toolHTML = '<div id="' + toolsArray[index][_elementID] + '" class="measurementToolClass" onmousedown="moveToTop(this.id)">' +
+						'<img id="' + toolsArray[index][_toolImageID] + '" src="">'+
+						'<p id="' + toolsArray[index][_elementID] + '_DragHelp" class="toolHelpText_Drag">Drag this into position</p>'+
+						'<p id="' + toolsArray[index][_elementID] + '_ResizeHelp" class="toolHelpText_Resize">Drag a corner or edge to calibrate</p>'+
+						'</div>';
+		//Appending the tool's HTML
+		mainDiv.insertAdjacentHTML("afterBegin", toolHTML);
+		
+		//HTML code for the tools button
+		var toolButtonHTML = 
+							 '<button ' +
+//							 'value="' + toolsArray[index][_toolButtonTitle] + '" ' +
+							 'id="' + toolsArray[index][_measureToolButton] + 
+							 '" class="measurementToolClass" ' + 
+//							 'style="padding: 25px 20px;" ' +
+							 'onclick="hideTool(\'#' + toolsArray[index][_elementID] + '\')">' +
+							 toolsArray[index][_toolButtonTitle] + '</button>';
+		//Appenging the tool's button HTML
+		toolButtonTable.insertAdjacentHTML("beforeEnd", toolButtonHTML);
+		
+		index++;
+	}
+	
+	//Reset Tool Button
+	//If there are any tools?
+	if(toolsArray.length == 1){
+		var resetButtonHTML = '<button id="resetTools" onclick="resetTools()">Reset Tool</button>';
+		toolButtonTable.insertAdjacentHTML("beforeEnd", resetButtonHTML);
+	}
+	else if(toolsArray.length > 1){
+		var resetButtonHTML = '<button id="resetTools" onclick="resetTools()">Reset Tools</button>';
+		toolButtonTable.insertAdjacentHTML("beforeEnd", resetButtonHTML);
+	}
+	
+	//Options are in the toolsArray array
+	var index = 0;
+	var tool;
+	var tool_jQuery;
+	
+	//Stopwatch
+	var stopWatch_Tool = $("#stopWatchTest");
+	
+	//Removing the stop watch if it disabled
+	if(!stopWatch){
+		stopWatch_Tool.css("display", "none");
+	}
+	
+	$(".measurementToolClass").hide();
+	
+//	var toolButton;
+//	//Hiding all the tools
+//	for(elements in allToolsArray){
+//		tool_jQuery = $(document.getElementById(allToolsArray[index][0]));
+//		tool_jQuery.css("display", "none");
+//		toolButton = $(document.getElementById(allToolsArray[index][1]));
+//		toolButton.css("display", "none");
+//		index++;
+//	}
+	
+	//Tools table
+	if(toolsArray.length == 0){
+		$("#button_table_2").css("display", "none");
+	}	
+	
+	index = 0;
+	//Showing only the tools that are defined in toolsArray
+	for(elements in toolsArray){
+		
+		//debug
+//		console.log("adding in the tools");
+//		console.log(toolsArray[index][0]);
+		
+		//Getting the tool
+		tool_jQuery = $(document.getElementById(toolsArray[index][_elementID]));
+		tool = document.getElementById(toolsArray[index][_elementID]);
+		toolButton = $(document.getElementById(toolsArray[index][_measureToolButton]));
+
+		//Setting the default location for the tool
+		tool.style.left = toolsArray[index][_positionLeft];
+		tool.style.top = toolsArray[index][_positionTop];
+
+		//Setting the default size of the tool
+		tool.style.height = toolsArray[index][_sizeHeight];
+		tool.style.width = toolsArray[index][_sizeWidth];
+
+		//Show the tool?
+		if(toolsArray[index][_show]){
+			tool_jQuery.css("display", "initial");
+		}
+
+		//Show the button for the tool
+		toolButton.css("display", "initial");
+		
+		//Draggable?
+		if(toolsArray[index][_draggable]){
+			if(toolsArray[index][_contained]){
+				tool_jQuery.draggable({containment:"body"});
+			}
+			else{
+				tool_jQuery.draggable();
+			}
+		}
+
+		//Resizable?
+		if(toolsArray[index][_resizable]){
+			tool_jQuery.resizable({
+									aspectRatio:toolsArray[index][_aspectRatioLocked],
+									handles:toolsArray[index][_resizableSides]
+									});
+		}
+
+		//Z-Index
+		tool_jQuery.css("z-index", toolsArray[index][_zIndex]);
+
+		//Position Attributes
+		tool_jQuery.css("position", "absolute");
+
+		//Color
+		tool_jQuery.css("background-color", toolsArray[index][_color]);
+		tool_jQuery.css("border", "1px solid black");
+
+		//Showing Help Text
+		//Draggable
+		if(!toolsArray[index][_draggableHelpText]){
+			$("#"+toolsArray[index][_elementID]+"_DragHelp").css("display", "none");
+		}
+		//Resizable
+		if(!toolsArray[index][_resizeHelpText]){
+			$("#"+toolsArray[index][_elementID]+"_ResizeHelp").css("display", "none");
+		}
+		
+		//Binding the tools to the drag and resize callbacks
+		tool_jQuery.on( "drag", function( event, ui ) {$("#"+this.id+"_DragHelp").css("display", "none");} );
+		tool_jQuery.on( "resize", function( event, ui ) {$("#"+this.id+"_ResizeHelp").css("display", "none");} );
+		
+		//Setting up the images for the tool
+		document.getElementById(toolsArray[index][_toolImageID]).src = toolsArray[index][_toolImage];
+		
+		index++;
+	}
+	
+	//Setting the attributes for the measurement tools help text
+	//Drag Text
+	var toolDragText = document.getElementsByClassName('toolHelpText_Drag');
+	for(var i = 0; i < toolDragText.length; i++) {
+		toolDragText[i].style.fontSize = toolHelpTextSize;
+		toolDragText[i].style.color = toolHelpTextColor;
+	}
+	//Resize Text
+	var toolResizeText = document.getElementsByClassName('toolHelpText_Resize');
+	for(var i = 0; i < toolResizeText.length; i++) {
+		toolResizeText[i].style.fontSize = toolHelpTextSize;
+		toolResizeText[i].style.color = toolHelpTextColor;
+	}
+	
+	//Overlay Image Button
+	if(!enableOverlayImage){
+		$("#overlayImageButton").css("display", "none");
+	}
+	else{
+		console.log(document.getElementById("video_selector").selectedIndex);
+		document.getElementById("overlayImageID").src = videoArray[document.getElementById("video_selector").selectedIndex][_overlayImage];
+		$("#overlayImageID").css("display", "none");
+	}
+});
