fe874e3198d9802a6ccdce9498601fd3bda3d753
Jared Poetter <Jared@Jared-Poetters-iMac.local>
2015-01-31 13:57:58 -0600

    cleaned up all of the code to make it easier to read and develop. also i took out all of the console messages and debug code that was commented out.

--- genXXXX/javascript/playerControls.js
+++ genXXXX/javascript/playerControls.js
@@ -1,94 +1,99 @@
-//creates variable called percent
 var percent;
-//creates variable called oldVal
 var oldVal;
-//called when "Go!" button is pushed (see DMVPlayer.html)
+//------------------------------------------------------------------//
+//This function handles the 'Go to Frame:' functionality of the     //
+// player.                                                          //
+//------------------------------------------------------------------//
 function goToEnteredFrame(){ 
-	//prints "Going To Frame:" in the console, for debugging
-//	console.log("Going To Frame:"); 
-	//gets value from textbox (named FrameJump) and sets it as a variable called thisFrame
-	var thisFrame = document.getElementById("FrameJump").value; 
-	//prints that value in the console (for debugging)
-//	console.log(thisFrame); 
-	//convers frame number into a rounded decimal video time
-	seek(Math.round(((1/framerate)*(thisFrame)+(1/framerate))*100000)/100000); 
-	//pause the video
-	pause(); 
-	//adds one to oldVal, so that it no longer equals newVal and the slider consequently updates (see scrubber.js)
-	++oldVal; 
-//	console.log("go to frame:"+thisFrame);
-  
-    //Deselecting the Go To Frame button and its text field
-    document.getElementById('FrameJump').blur();
-    document.getElementById('gotoframe').blur();
+  //gets value from textbox (named FrameJump) and sets it as a variable called thisFrame
+  var thisFrame = document.getElementById("FrameJump").value; 
+  //convers frame number into a rounded decimal video time
+  seek(Math.round(((1/framerate)*(thisFrame)+(1/framerate))*100000)/100000); 
+  //pause the video
+  pause(); 
+  //adds one to oldVal, so that it no longer equals newVal and the slider consequently updates (see scrubber.js)
+  ++oldVal; 
+  //Deselecting the Go To Frame button and its text field
+  document.getElementById('FrameJump').blur();
+  document.getElementById('gotoframe').blur();
 }
 
+//------------------------------------------------------------------//
+//Event Handler for the keyboard input                              //
+//------------------------------------------------------------------//
 //isaac wrote this. listens for keydownn event and also for which key is pressed
 window.addEventListener('keydown',this.check,false); 
 //runs if the keydown event is fired, not sure what e is
 function check(e) { 
-	//figures out which key is presed and set it as variable 'code'
-	var code = e.keyCode; 
-	//Left arrow pressed
-	if (code == 37)
-		prevFrame();
-	//Right arrow pressed
-	if (code == 39)
-		fwdFrame();
-	//Down arrow pressed
-	if (code == 40)
-		prevFrame();
-	//Up arrow pressed
-	if (code == 38)
-		fwdFrame();
-	//Spacebar pressed
-	if (code == 32)
-		playPause();
-	}
+  //figures out which key is presed and set it as variable 'code'
+  var code = e.keyCode; 
+  //Left arrow pressed
+  if (code == 37)
+    prevFrame();
+  //Right arrow pressed
+  if (code == 39)
+    fwdFrame();
+  //Down arrow pressed
+  if (code == 40)
+    prevFrame();
+  //Up arrow pressed
+  if (code == 38)
+    fwdFrame();
+  //Spacebar pressed
+  if (code == 32)
+    playPause();
+}
 
+//------------------------------------------------------------------//
+//This function is used to go to the previous frame of the video.   //
+// The function is executed when the previous frame button or key is// 
+//  pressed.                                                        //
+//------------------------------------------------------------------//
 function prevFrame(){
-	//changing the value of oldVal allows the slider to be updated
-	++oldVal; 
-	//selects previous frame button
-	document.getElementById("frameprev").focus(); 
-	pause();
-	//gets current time to 5 decimal places
+  //changing the value of oldVal allows the slider to be updated
+  ++oldVal; 
+  //selects previous frame button
+  document.getElementById("frameprev").focus(); 
+  pause();
+  //gets current time to 5 decimal places
     if (numberOfVideos == 1 || numberOfVideos == 2){
       player.currentTime -= (1/30).toPrecision(5);
     }
     if (numberOfVideos == 2){
       player2.currentTime -= (1/30).toPrecision(5);
     }
-	
-//	var frameNumber = player.currentTime / 30;
-//	console.log("(1/30).toPrecision(5): "+(1/30).toPrecision(5));
-//	console.log("current time: "+player.currentTime);
-//	console.log("current frame: "+frameNumber);
-	
-//	console.log("prev frame");
-	//deselects previous frame button
-	document.getElementById("frameprev").blur();
+  //deselects previous frame button
+  document.getElementById("frameprev").blur();
 }
 
+//------------------------------------------------------------------//
+//This function is used for playing and pausing the video.          //
+// The function is executed when the play/pause button is pressed or//
+//  when the space bar is pressed.                                  //
+//------------------------------------------------------------------//
 function playPause(){ 
-	//the next two lines deselct everything
-    if (goToFrameControlEnabled) {
-	   document.getElementById("gotoframe").focus();
-	   document.getElementById("gotoframe").blur();
-    }
+  //the next two lines deselct everything
+  if (goToFrameControlEnabled) {
+   document.getElementById("gotoframe").focus();
+   document.getElementById("gotoframe").blur();
+  }
       
-	if (player.paused && player2.paused) {
-		play();
-	}
-	else {
-		doNotUpdate=1;
-		pause();
-	}
+  if (player.paused && player2.paused) {
+    play();
+  }
+
+  else {
+    doNotUpdate=1;
+    pause();
+  }
   
-    //Deselecting the Play/Pause Button
-    document.getElementById('playorpause').blur();
+  //Deselecting the Play/Pause Button
+  document.getElementById('playorpause').blur();
 } 
 
+//------------------------------------------------------------------//
+//This function is the pause function that pauses the video(s)      //
+//------------------------------------------------------------------//
 function pause(){
     if (numberOfVideos == 1 || numberOfVideos == 2){
       player.pause();
@@ -96,8 +101,11 @@ function pause(){
     if (numberOfVideos == 2) {
       player2.pause();
     }
-//	console.log("pause");
 }
+
+//------------------------------------------------------------------//
+//This function is the play function that plays the video(s)        //
+//------------------------------------------------------------------//
 function play(){
     if (numberOfVideos == 1 || numberOfVideos == 2){
       player.play();
@@ -105,19 +113,28 @@ function play(){
     if (numberOfVideos == 2) {
       player2.play();
     }
-//	console.log("play");
 }
-//can be called by seek(10), seeks to 10 seconds
+
+//------------------------------------------------------------------//
+//This function is used to seek to a specific time in the video.    //
+//Parameters: time - the time in seconds that you want to move to in//
+//                    the video                                     //
+//------------------------------------------------------------------//
 function seek(time){ 
-	pause();
+  pause();
     if (numberOfVideos == 1 || numberOfVideos == 2) {
       player.currentTime=time;
     }
     if (numberOfVideos == 2) {
       player2.currentTime = time;
     }
-//	console.log("seek");
 }
+
+//------------------------------------------------------------------//
+//This function is used to seek to a specific time in the video.    //
+//Parameters: time - the time in seconds that you want to move to in//
+//                    the video                                     //
+//------------------------------------------------------------------//
 function goToTime(time){
     if (numberOfVideos == 1 || numberOfVideos == 2) {
       player.currentTime=time;
@@ -125,54 +142,51 @@ function goToTime(time){
     if (numberOfVideos == 2) {
       player2.currentTime=time;
     }
-//	console.log("goToTime:" + time);
 }
+
+//------------------------------------------------------------------//
+//This function is used to get back to the first frame in the video //
+//------------------------------------------------------------------//
 function firstFrame(){
-	++oldVal;
-	goToTime(0);
+  ++oldVal;
+  goToTime(0);
     
-    //Deselecting the Rewind Button
-    document.getElementById('rewind').blur();
+  //Deselecting the Rewind Button
+  document.getElementById('rewind').blur();
 }
+
+//------------------------------------------------------------------//
+//This function is used to move ahead a single frame in the video   //
+//------------------------------------------------------------------//
 function fwdFrame(){
-	++oldVal;
-	doingSomethingElse=1;
-	mousePos=0;
-	sliding = 0;
-	//isChrome set true or false in browser detect file
-	if (isChrome){ 
-//		console.log("You are using chrome, I will skip by playing");
-		play();
-		//set timeout calls a function after a certain amount of time
-		setTimeout(function() {player.pause(); player2.pause()}, (1000/framerate)); 
-//		console.log("fwd frame Chrome, time: "+player.currentTime);
-		doingSomethingElse=0;
-
-		}
-	else{
-//		console.log("Skip by setting time");
-		pause();
-        if (numberOfVideos == 1 || numberOfVideos == 2) {
-          player.currentTime=player.currentTime + (1/framerate);
-        }
-        if (numberOfVideos == 2) {
-          player2.currentTime = player2.currentTime + (1/framerate);
-        }
-//		console.log("fwd frame other browser, time: "+player.currentTime);
-		doingSomethingElse=0;
-
-	}
-	document.getElementById("framefwd").blur();
-//	console.log("fwd frame , time: "+player.currentTime);
-	
-//	var frameNumber = player.currentTime / 30;
-//	console.log("current time: "+player.currentTime);
-//	console.log("current frame: "+frameNumber);
-	
+  ++oldVal;
+  doingSomethingElse=1;
+  mousePos=0;
+  sliding = 0;
+  //isChrome set true or false in browser detect file
+  if (isChrome){ 
+    play();
+    //set timeout calls a function after a certain amount of time
+    setTimeout(function() {player.pause(); player2.pause()}, (1000/framerate)); 
+    doingSomethingElse=0;
+  }
+  else{
+    pause();
+    if (numberOfVideos == 1 || numberOfVideos == 2) {
+      player.currentTime=player.currentTime + (1/framerate);
+    }
+    if (numberOfVideos == 2) {
+      player2.currentTime = player2.currentTime + (1/framerate);
+    }
+    doingSomethingElse=0;
+  }
+  document.getElementById("framefwd").blur();
 }
 
-//Start button function
+//------------------------------------------------------------------//
+//This function is used to start the video                          //
+//------------------------------------------------------------------//
 function start(){
-	play();
-	pause();
+  play();
+  pause();
 }
--- genXXXX/javascript/scrubber.js
+++ genXXXX/javascript/scrubber.js
@@ -10,107 +10,99 @@
 var value;
 
 $(function() {
-	//----Variables--------------------------------------------//
-	//creates an jquery instance of the video player window
-	var $video = $("#dmv_video"); 
-    var $video2 = $("dmv_video2");
-	//creates an instance of the jquery object $video
-	var video = $video.get(0);
+  //----Variables--------------------------------------------//
+  //creates an jquery instance of the video player window
+  var $video = $("#dmv_video"); 
+  var $video2 = $("dmv_video2");
+  //creates an instance of the jquery object $video
+  var video = $video.get(0);
     
-	//variable used to indicate whether or not the playpause button has been clicked
-	var playPauseClicked; 
-	//0 is mouse up 1 is mouse down, the initial value is set to zero
-	var mousePos=0; 
-	//is set to one if the slider is not to be allowed to update
-	var doNotUpdate = 0; 
-	//if the slider is clicked on, this variable is given the time that the video should go to depending on where the slider bar was moved to
-	var clickedScrubberTime; 
-	
-	//there is an event which 'fires' every so often when the video is playing. it is called 'timeupdate' and what this line does is it says that anytime this event fires, the function videoTImeUpdateHandler should be called. This function updates the slider position
-	$video.bind("timeupdate", videoTimeUpdateHandler); 
-    $video2.bind("timeupdate", videoTimeUpdateHandler);
-	
-	//gets the percent of the video that is done playing, this can be applied to the slider to set its position
-	var percent = $video.currentTime / $video.duration; 
-	
-    // Create a new jQuery UI Slider element and set some default parameters.
-    $( "#slider" ).slider({ 
-    	range: "min", 
-      	//initial value of the slider should be zero
-		value: 0, 
-		// this is teh minimum value of the slider
-      	min: 0, 
-		//this is the max slider position, this is effectively the resolution of the slider. the 100,000 means that it can handle 100,000 different positions, or videos with 100,000 frames which is more than enough. this value can be increased or decreased.
-      	max: maxSliderValue, 
-		//the slide: is an event that fires when the slider is being moved. the function() inside runs whenever the slider is being moved
-      	slide: function( event, ui ) {
-			// this gets the value of the slider, a number between 0 and 100,000
-			var value = $( "#slider" ).slider( "value" ); 
-			//gets the percentage value of the slider, and then sets the current time of the video according to where the slider is positioned
-            if (numberOfVideos == 1 || numberOfVideos == 2) {
-			   player.currentTime=player.duration*(value/maxSliderValue); 
-            }
-            if (numberOfVideos == 2) {
-			   player2.currentTime = player2.duration * (value/maxSliderValue);
-            }
-			//prints the value of the slider in the console, used only for debugging
-			//console.log(value); 
-      	}
-	});
-	
-	//retreives the slider object
-	$( "#slider" ).slider({ 
-		//function runs when the slider is released and is no longer being slid (slided?)
-		stop: function( event, ui ) { 
-			//selects (puts focus on the gotoframe textbox (had to pick just an arbitrary object)
-			document.getElementById("gotoframe").focus(); 
-			//deselects the textbox, this may seem redundant given the code above, but what it does is makes sure that nothing on the page is selected because when the spacebar is pressed, anything that is selected (has focus) will be triggered, or 'clicked' on
-			document.getElementById("gotoframe").blur(); 
-			//gets final value of slidr after it is not being moved anymore
-			var value = $( "#slider" ).slider( "value" ); 
-			//gets percentage value of the slider
-            if (numberOfVideos == 1 || numberOfVideos == 2) {
-              player.currentTime=player.duration*(value/maxSliderValue);
-            }
-            if (numberOfVideos == 2) {
-              player2.currentTime = player2.duration * (value / maxSliderValue);
-            }
-            
-			//prints the slider value in the console, used for debugging only
-//			console.log("Slider Value: "+value);
-//            console.log("Video 1 Current Time: "+player.currentTime);
-//            console.log("Video 2 Current Time: "+player2.currentTime);
-		}
-	});
-	
-	//Debugging
-	//console.log(document.getElementById("slider").offsetWidth);
-	//sets varibale called newVal to the slider value
-	var newVal=$( "#slider" ).slider( "value" ); 
-	
-	//the value of oldVal is set eslewhere in the playerControls.js file
-	function videoTimeUpdateHandler(e) {
-		// if (newVal does not equal oldVal OR player is NOT paused, then run this. this test checks to see if the video has changed or is changing positions. the following code runs if it is.
-		if((newVal!=oldVal) || !(player.paused) || !(player.paused)){ 
-			//set variable called value to the slider value
-			value = $( "#slider" ).slider( "value" ); 
-			//get percent of the video that has palyed
-			percent = video.currentTime / video.duration; 
-			//set the position of the slider based on how far along in the video we are.
-			$( "#slider" ).slider( "option", "value", percent * maxSliderValue ); 
+  //variable used to indicate whether or not the playpause button has been clicked
+  var playPauseClicked; 
+  //0 is mouse up 1 is mouse down, the initial value is set to zero
+  var mousePos=0; 
+  //is set to one if the slider is not to be allowed to update
+  var doNotUpdate = 0; 
+  //if the slider is clicked on, this variable is given the time that the video should go to depending on where the slider bar was moved to
+  var clickedScrubberTime; 
+  
+  //there is an event which 'fires' every so often when the video is playing. it is called 'timeupdate' and what this line does is it says that anytime this event fires, the function videoTImeUpdateHandler should be called. This function updates the slider position
+  $video.bind("timeupdate", videoTimeUpdateHandler); 
+  $video2.bind("timeupdate", videoTimeUpdateHandler);
+  
+  //gets the percent of the video that is done playing, this can be applied to the slider to set its position
+  var percent = $video.currentTime / $video.duration; 
+  
+  // Create a new jQuery UI Slider element and set some default parameters.
+  $( "#slider" ).slider({ 
+    range: "min", 
+    //initial value of the slider should be zero
+    value: 0, 
+    // this is teh minimum value of the slider
+    min: 0, 
+    //this is the max slider position, this is effectively the resolution of the slider. the 100,000 means that it can handle 100,000 different positions, or videos with 100,000 frames which is more than enough. this value can be increased or decreased.
+    max: maxSliderValue, 
+    //the slide: is an event that fires when the slider is being moved. the function() inside runs whenever the slider is being moved
+    slide: function( event, ui ) {
+      // this gets the value of the slider, a number between 0 and 100,000
+      var value = $( "#slider" ).slider( "value" ); 
+      //gets the percentage value of the slider, and then sets the current time of the video according to where the slider is positioned
+      if (numberOfVideos == 1 || numberOfVideos == 2) {
+        player.currentTime=player.duration*(value/maxSliderValue); 
+      }
+      if (numberOfVideos == 2) {
+        player2.currentTime = player2.duration * (value/maxSliderValue);
+      }
+    }
+  });
+  
+  //retreives the slider object
+  $( "#slider" ).slider({ 
+    //function runs when the slider is released and is no longer being slid (slided?)
+    stop: function( event, ui ) { 
+      //selects (puts focus on the gotoframe textbox (had to pick just an arbitrary object)
+      document.getElementById("gotoframe").focus(); 
+      //deselects the textbox, this may seem redundant given the code above, but what it does is makes sure that nothing on the page is selected because when the spacebar is pressed, anything that is selected (has focus) will be triggered, or 'clicked' on
+      document.getElementById("gotoframe").blur(); 
+      //gets final value of slidr after it is not being moved anymore
+      var value = $( "#slider" ).slider( "value" ); 
+      
+      //gets percentage value of the slider
+      if (numberOfVideos == 1 || numberOfVideos == 2) {
+        player.currentTime=player.duration*(value/maxSliderValue);
+      }
+      if (numberOfVideos == 2) {
+        player2.currentTime = player2.duration * (value / maxSliderValue);
+      }
+    }
+  });
+  
+  //sets varibale called newVal to the slider value
+  var newVal=$( "#slider" ).slider( "value" ); 
+  
+  //the value of oldVal is set eslewhere in the playerControls.js file
+  function videoTimeUpdateHandler(e) {
+    // if (newVal does not equal oldVal OR player is NOT paused, then run this. this test checks to see if the video has changed or is changing positions. the following code runs if it is.
+    if((newVal!=oldVal) || !(player.paused) || !(player.paused)){ 
+      //set variable called value to the slider value
+      value = $( "#slider" ).slider( "value" ); 
+      //get percent of the video that has palyed
+      percent = video.currentTime / video.duration; 
+      //set the position of the slider based on how far along in the video we are.
+      $( "#slider" ).slider( "option", "value", percent * maxSliderValue ); 
 
-			//sets old val equall to newVal so that this peice of code cannot be run twice without something changing
-			oldVal=newVal;
-			
-			//Stopwatch
-			stopwatch();
-		}
-	}
+      //sets old val equall to newVal so that this peice of code cannot be run twice without something changing
+      oldVal=newVal;
+      
+      //Stopwatch
+      stopwatch();
+    }
+  }
 });
 
 //Update the stopwatch time
 function stopwatch(){
-//	console.log("stopwatch");
-//	console.log(player.currentTime);
-//	document.getElementById('stopWatchTest').innerHTML = player.currentTime;
+//  console.log("stopwatch");
+//  console.log(player.currentTime);
+//  document.getElementById('stopWatchTest').innerHTML = player.currentTime;
 }
